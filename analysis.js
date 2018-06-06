3caff3c896b3e83f3ba21b1000ab00cfb101756f
rcanoff <rcanoff@gmail.com>
2018-06-06 18:01:46 -0300

    Changed the comments

--- analysis.js
+++ analysis.js
@@ -1,68 +1,88 @@
-'use strict';
 const Analysis = require('tago/analysis');
 const Utils    = require('tago/utils');
 const Device   = require('tago/device');
-/**
- * The follow function is our main function, that will call ãll others.
- * The function presumes that you have a enviroment variable, that can be set in the admin website, in the Analysis configuration:
- * -> device_token: token
- * @param  {object} context automatic received from Tago
- */
-function run_analysis(context) {
-    //Convert the environment variables into an object.
-    //You can do this by yourself, we just added an easily way.
-    const env_vars  = Utils.env_to_obj(context.environment);
 
-    const my_device = new Device(env_vars.device_token);
+// To run this analysis you need to add a device token to the environment variables,
+// To do that, go to your device, then token and copy your token.
+// Go the the analysis, then environment variables, 
+// add a new one and type device_token on key, and paste your token on value
 
-    my_device.find({"variable":"temperature", "query":"min", "start_date":"1 day"}).then((result_array) => {
-        if (!result_array[0]) return context.log('No result for minimum calculation');
+// This analysis reads the minimum, maximum and average value of the variable "temperature"
+// then the analysis then saves this values in a new variable
 
-        //The API will always return an array of results.
-        //But, when using a "query" parameter, it will always return an array with only one result.
-        //So, we have no need to check for multiple values.
-        const obj_to_save = {
-            "variable": "temperature_minimum",
-            "value": result_array[0].value,
-            "unit": "F"
-        };
+// The function myAnalysis will run when you execute your analysis
+async function myAnalysis(context) {
+  // reads the values from the environment and saves it in the variable env_vars
+  const env_vars = Utils.env_to_obj(context.environment);
 
-        my_device.insert(obj_to_save).then(context.log("Temperature Minimum Updated"));
+  const device = new Device(env_vars.device_token);
+  // This is a filter to get the minimum value of the variable temperature in the last day
+  const minFilter = {
+    variable: 'temperature',
+    query: 'min',
+    start_date: '1 day',
+  };
 
-    }).catch(error => context.log(error)); //Just another way to get the error. Do exatcly same thing than the other above.
+  // Now we use the filter for the device to get the data
+  // check if the variable min has any value
+  // if so, we crete a new object to send to Tago
+  const [min] = await device.find(minFilter);
+  if (min) {
+    const minValue = {
+      variable: 'temperature_minimum',
+      value: min.value,
+      unit: 'F',
+    };
+  
+    // now we insert the new object with the minimum value
+    await device.insert(minValue).then(context.log('Temperature Minimum Updated'));
+  } else {
+    context.log('Minimum value not found');
+  }
 
-    my_device.find({"variable":"temperature", "query":"max", "start_date":"1 day"}).then((result_array) => {
-        if (!result_array[0]) return context.log('No result for maximum calculation');
-        const obj_to_save = {
-            "variable": "temperature_maximum",
-            "value": result_array[0].value,
-            "unit": "F"
-        };
-        my_device.insert(obj_to_save).then(context.log("Temperature Maximum Updated"));
+  // This is a filter to get the maximum value of the variable temperature in the last day
+  const maxFilter = {
+    variable: 'temperature',
+    query: 'max',
+    start_date: '1 day',
+  };
 
-    }).catch(context.log); //Pass context.log, it will print the error in the console so we can know that something is wrong.
+  const [max] = await device.find(maxFilter);
+  if (max) {
+    const maxValue = {
+        'variable': 'temperature_maximum',
+        'value': max.value,
+        'unit': 'F',
+    };
+    await device.insert(maxValue).then(context.log('Temperature Maximum Updated'));
+  } else {
+    context.log('Maximum value not found');
+  }
 
-    my_device.find({"variable":"temperature", "qty": 1000, "start_date":"1 day"}).then((result_array) => {
-        if (!result_array[0]) return context.log('No result for avg calculation');
+  // This is a filter to get the last 1000 values of the variable temperature in the last day
+  const avgFilter = {
+    variable: 'temperature',
+    qty: 1000,
+    start_date: '1 day',
+  };
 
-        //In this case, we are looking for, at maximum, 1.000 values, to get the avg temperature.
-        //I'm just presuming that you will have less than 1.000 records in the last day
-        //Remember that API only let you get 10.000 records by request.
-        let temperature_sum = result_array.reduce((final_value, current_obj) => {
-            return final_value + Number(current_obj.value);
-        }, 0);
-
-        temperature_sum = temperature_sum / result_array.length;
-
-        const obj_to_save = {
-            "variable": "temperature_average",
-            "value": temperature_sum,
-            "unit": "F"
-        };
-        my_device.insert(obj_to_save).then(context.log("Temperature Average Updated"));
-    }).catch(context.log);
-
-
+  const avg = await device.find(avgFilter);
+  if (avg.length) {
+    let temperatureSum = avg.reduce((previewsValue, currentValue) => {
+      return previewsValue + Number(currentValue.value);
+    }, 0);
+  
+    temperatureSum = temperatureSum / avg.length;
+  
+    const avgValue = {
+      'variable': 'temperature_average',
+      'value': temperatureSum,
+      'unit': 'F',
+    };
+    await device.insert(avgValue).then(context.log('Temperature Average Updated'));
+  } else {
+    context.log('No result found for the avg calculation');
+  }
 }
 
-module.exports = new Analysis(run_analysis, 'MY-ANALYSIS-TOKEN-HERE');
+module.exports = new Analysis(myAnalysis, 'MY-ANALYSIS-TOKEN-HERE');
