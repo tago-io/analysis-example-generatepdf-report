5ad0806c51fd6a8665102c10cd2f72d2964cd712
Felipe Lima <felipe@ferreiradelima.com>
2020-08-03 11:29:12 -0300

    update sdk

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 Generate a pdf file to send via email.
 
 
-## How to run the script on Tago
+## How to run the script on TagoIO
 Do your own modifications and add variable data from a device in your report.<br>
-Upload to Tago analysis, in the admin website.<br>
+Upload to TagoIO analysis, in the admin website.<br>
 Add the environment variable `email` with an email.
 Add the environment variable `device_token` with the device token of the device you would like to include in your report and the variable to be on line 21.
 
@@ -15,7 +15,7 @@ Add the environment variable `email` with a email.<br>
 Add the environment variable `device_token` with the device token of the device you would like to include in your report and the variable to be added where designated in the script.
 Open the analysis.js, change `MY-ANALYSIS-TOKEN-HERE` line for your analysis token.<br>
 Open the terminal and run:
- 
+
 
 `npm install`<br>
-`node .`
+`node analysis.js`
--- analysis.js
+++ analysis.js
@@ -1,42 +1,53 @@
-/* 
+/*
  * Analysis Example
  * Generate pdf report and send via email
- * 
- * 
+ *
+ *
  * Instructions
  * To run this analysis you need to add a email and device_token to the environment variables,
- * Go the the analysis, then environment variables, 
+ * Go the the analysis, then environment variables,
  * type email on key, and insert your email on value
  * type device_token on key and insert your device token on value
-*/
+ */
 
-const Analysis = require('tago/analysis');
-const TagoDevice = require('tago/device');
-const TagoAccount = require('tago/account');
-const Utils    = require('tago/utils');
-const Service  = require('tago/services');
-const axios    = require('axios');
-const moment   = require('moment-timezone');
+const { Analysis, Device, Services, Utils } = require("@tago-io/sdk");
+const axios = require("axios");
+const moment = require("moment-timezone");
 
-const your_variable = ''; //enter the variable from your device you would like in the report
+const your_variable = "your_variable"; //enter the variable from your device you would like in the report
 
 // The function myAnalysis will run when you execute your analysis
 async function myAnalysis(context) {
   // reads the values from the environment and saves it in the variable env_vars
-  const env_vars = Utils.env_to_obj(context.environment);
-  if (!env_vars.email) return context.log('email environment variable not found');
-  const tago_device = new TagoDevice(env_vars.device_token);
-  if (!env_vars.device_token) return context.log('device_token environment variable not found');
-  const data = await tago_device.find({ variable: ['your_variable','unit'], start_date: '10 year', qty: 1 });
-  let dataParsed = 'variable,value,unit,time';
+  const env_vars = Utils.envToJson(context.environment);
+
+  if (!env_vars.email) {
+    return context.log("email environment variable not found");
+  }
+
+  if (!env_vars.device_token) {
+    return context.log("device_token environment variable not found");
+  }
+
+  const device = new Device({ token: env_vars.device_token });
+
+  const data = await device.getData({
+    variable: [your_variable, "unit"],
+    start_date: "10 year",
+    qty: 1,
+  });
+
+  let dataParsed = "variable,value,unit,time";
+
   data.forEach((x) => {
     dataParsed = `${x.variable},${x.value},${x.unit},${x.time}`;
   });
-  const dataArray = dataParsed.split(',');
-  const dataVar = dataArray[0];
-  const dataVal = dataArray[1]; 
 
-const html = `<html>
+  const dataArray = dataParsed.split(",");
+  const dataVar = dataArray[0];
+  const dataVal = dataArray[1];
+
+  const html = `<html>
   <head>
       <style>
           body, html {
@@ -55,78 +66,73 @@ const html = `<html>
       </style>
   </head>
   <body>
-      <table>
-          <tr>
-              <td colspan="7">Issue date: ${moment().format('YYYY-MM-DD HH:mm:ss')}</td>
-          </tr>
-          <tr>
-              <td colspan="4">Start date: 2020-05-20 10:21:32</td>
-              <td colspan="3">Stop date: 2020-10-08 22:56:19</td>
-          </tr>
-          <tr>
-              <td colspan="4"> Report of the ${dataVar}</td>
-              <td colspan="3">Device Kitchen Oven 5</td>
-          </tr>
-          <tr>
-              <td>Counter</td>
-              <td>${dataVar}</td>
-              <td>Time</td>
-              <td>Date</td>
-              <td>Temperature 2</td>
-              <td>Time</td>
-              <td>Date</td>
-          </tr>
-
-          <tr>
-          <td>2</td>
-          <td>${dataVal}</td>
-          <td>10:53:20</td>
-          <td>2020-06-10</td>
-          <td>137</td>
-          <td>10:53:20</td>
-          <td>2020-06-10</td>
-        </tr>
-
-    </tr>
-      </table>
+    <table>
+      <tr>
+          <td colspan="7">Issue date: ${moment().format("YYYY-MM-DD HH:mm:ss")}</td>
+      </tr>
+      <tr>
+          <td colspan="4">Start date: 2020-05-20 10:21:32</td>
+          <td colspan="3">Stop date: 2020-10-08 22:56:19</td>
+      </tr>
+      <tr>
+          <td colspan="4"> Report of the ${dataVar}</td>
+          <td colspan="3">Device Kitchen Oven 5</td>
+      </tr>
+      <tr>
+          <td>Counter</td>
+          <td>${dataVar}</td>
+          <td>Time</td>
+          <td>Date</td>
+          <td>Temperature 2</td>
+          <td>Time</td>
+          <td>Date</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>${dataVal}</td>
+        <td>10:53:20</td>
+        <td>2020-06-10</td>
+        <td>137</td>
+        <td>10:53:20</td>
+        <td>2020-06-10</td>
+      </tr>
+    </table>
   </body>
 </html>`;
 
-
-
   const options = {
     displayHeaderFooter: true,
-    footerTemplate: '<div class="page-footer" style="width:100%; text-align:center; font-size:12px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
+    footerTemplate:
+      '<div class="page-footer" style="width:100%; text-align:center; font-size:12px;">Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>',
     margin: {
-      top: '1.5cm',
-      right: '1.5cm',
-      left: '1.5cm',
-      bottom: '1.5cm',
+      top: "1.5cm",
+      right: "1.5cm",
+      left: "1.5cm",
+      bottom: "1.5cm",
     },
   };
-  const result = await axios.post('https://pdf.tago.io', { base64: Buffer.from(html).toString('base64'), options }).catch(context.log);
+
+  const base64 = Buffer.from(html).toString("base64");
+  const result = await axios.post("https://pdf.tago.io", { base64, options });
   const pdf = result.data.result;
 
   // Start the email service
-  const email = new Service(context.token).email;
-
-  // Pass to .send function the email addres, subject, body_msg, from and attachment.
-  // See the docs to know what is optional.
-  const email_address = env_vars.email;
-  const subject       = 'Exported File from Tago';
-  const body_msg      = 'This is an example of a body message';
-  const from          = 'tago@tago.io';
-  const attachment    = {
-    archive: pdf,
-    type: 'base64',
-    filename: 'exportedfile.pdf',
-  };
+  const email = new Services({ token: context.token }).email;
 
   // Send the email.
-  await email.send(email_address, subject, body_msg, from, attachment).then(context.log);
+  await email.send({
+    to: env_vars.email,
+    subject: "Exported File from TagoIO",
+    message: "This is an example of a body message",
+    attachment: {
+      archive: pdf,
+      type: "base64",
+      filename: "exportedfile.pdf",
+    },
+  });
 }
 
-// The analysis token in only necessary to run the analysis outside Tago
-module.exports = new Analysis(myAnalysis, 'if-run-external-insert-the-analysis-token-here');
+module.exports = new Analysis(myAnalysis);
 
-
+// To run analysis on your machine (external)
+// module.exports = new Analysis(myAnalysis, { token: "YOUR-TOKEN" });
